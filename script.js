e499f809739a482e28d2049371cf9e25f2c9383b
crochet-andi-mid <138617611+crochet-andi-mid@users.noreply.github.com>
2023-07-04 19:54:44 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 function sendWhatsApp(phoneNumber, productName, imageName) {
   var message = "¡Hola! Estoy interesado en el " + productName + ". ¿Podrías proporcionarme más información?";
-  var url = "https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + encodeURIComponent(message);
+  var url = "https://wa.me/" + phoneNumber + "?text=" + encodeURIComponent(message);
 
-  var link = document.createElement('a');
+  var link = document.createElement("a");
   link.href = url;
   link.target = "_blank";
+  document.body.appendChild(link);
   link.click();
-
-  setTimeout(function() {
-    var formData = new FormData();
-    formData.append("image", imageName);
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://api.whatsapp.com/send");
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.send(formData);
-  }, 3000);
 }
+
+var likeButtons = document.querySelectorAll('.like-button');
+likeButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    var productCard = this.parentNode.parentNode;
+    var likeCount = productCard.querySelector('.like-count');
+    var currentLikes = parseInt(likeCount.textContent);
+    likeCount.textContent = currentLikes + 1;
+  });
+});
+
+var reviewButtons = document.querySelectorAll('.review-button');
+reviewButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    var productCard = this.parentNode.parentNode;
+    var reviewCount = productCard.querySelector('.review-count');
+    var currentReviews = parseInt(reviewCount.textContent);
+    reviewCount.textContent = currentReviews + 1;
+  });
+});
