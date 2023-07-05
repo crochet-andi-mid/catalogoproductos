a375a373e09943904312355c1a11ce4bedabc7b3
crochet-andi-mid <138617611+crochet-andi-mid@users.noreply.github.com>
2023-07-04 19:06:16 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-// Obtener todos los botones de "Contactar por WhatsApp"
+// Obtener todos los botones de "WhatsApp" en las tarjetas de producto
 var whatsappButtons = document.querySelectorAll('.whatsapp-button');
 
-// Agregar un manejador de eventos a cada botón de "Contactar por WhatsApp"
+// Agregar un manejador de eventos a cada botón de "WhatsApp"
 whatsappButtons.forEach(function(button) {
   button.addEventListener('click', function() {
     var productCard = this.parentNode.parentNode;
-    var nombreProducto = productCard.querySelector('h2').textContent;
-    var precio = productCard.querySelector('.price').textContent;
-    var descripcion = productCard.querySelector('.description').textContent;
+    var productName = productCard.querySelector('h2').textContent;
+    var productPrice = productCard.querySelector('.price').textContent;
+    var productDescription = productCard.querySelector('.description').textContent;
+    var productImage = productCard.querySelector('img').src;
 
-    enviarWhatsapp('+529844334844', nombreProducto, precio, descripcion);
+    // Modificar la URL de la imagen para que sea relativa al archivo HTML
+    var imageURL = window.location.href.replace('index.html', productImage);
+
+    var whatsappURL = 'https://api.whatsapp.com/send?text=' + encodeURIComponent('¡Hola! Estoy interesado en el producto ' + productName + '. Precio: ' + productPrice + '. Descripción: ' + productDescription + '. Adjunto una imagen: ' + imageURL);
+
+    // Abrir la ventana de WhatsApp con el mensaje predefinido
+    window.open(whatsappURL);
   });
 });
-
-// Función para enviar un mensaje de WhatsApp
-function enviarWhatsapp(numero, nombreProducto, precio, descripcion) {
-  var mensaje = "¡Hola! Estoy interesado en el producto: " +
-    nombreProducto +
-    "%0A%0APrecio: " +
-    precio +
-    "%0A%0ADescripción: " +
-    descripcion +
-    "%0A%0A¿Podrías brindarme más información?";
-
-  var url = "https://api.whatsapp.com/send?phone=" + encodeURIComponent(numero) + "&text=" + encodeURIComponent(mensaje);
-  window.open(url);
-}
