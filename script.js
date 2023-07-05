da9f0b40f064003f3445d8d5edd3fe1843daa744
crochet-andi-mid <138617611+crochet-andi-mid@users.noreply.github.com>
2023-07-04 19:23:03 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
-// Obtener todos los botones de "WhatsApp" en las tarjetas de producto
-var whatsappButtons = document.querySelectorAll('.whatsapp-button');
-
-// Agregar un manejador de eventos a cada botón de "WhatsApp"
-whatsappButtons.forEach(function(button) {
-  button.addEventListener('click', function() {
-    var productCard = this.parentNode.parentNode;
-    var productName = productCard.querySelector('h2').textContent;
-    var productPrice = productCard.querySelector('.price').textContent;
-    var productDescription = productCard.querySelector('.description').textContent;
-    var productImage = productCard.querySelector('img').src;
-
-    // Crear un enlace dinámico con los datos del producto
-    var whatsappLink = document.createElement('a');
-    whatsappLink.href = 'https://api.whatsapp.com/send?text=' + encodeURIComponent('¡Hola! Estoy interesado en el producto ' + productName + '. Precio: ' + productPrice + '. Descripción: ' + productDescription);
-    whatsappLink.target = '_blank';
-
-    // Crear una imagen oculta para adjuntarla al enlace de WhatsApp
-    var imageElement = document.createElement('img');
-    imageElement.src = productImage;
-    imageElement.style.display = 'none';
-
-    // Adjuntar la imagen al enlace de WhatsApp
-    whatsappLink.appendChild(imageElement);
-
-    // Simular un clic en el enlace de WhatsApp para abrir la aplicación
-    var event = new MouseEvent('click', {
-      view: window,
-      bubbles: false,
-      cancelable: true
-    });
-    whatsappLink.dispatchEvent(event);
+function sendWhatsApp(phoneNumber, productName, imageName) {
+  var message = "¡Hola! Estoy interesado en el " + productName + ". ¿Podrías proporcionarme más información?";
+  var url = "https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + encodeURIComponent(message);
+  window.open(url);
+  
+  var image = new Image();
+  image.src = imageName;
+  image.addEventListener('load', function() {
+    var canvas = document.createElement('canvas');
+    canvas.width = image.width;
+    canvas.height = image.height;
+    var context = canvas.getContext('2d');
+    context.drawImage(image, 0, 0);
+    var dataURL = canvas.toDataURL('image/png');
+    var link = document.createElement('a');
+    link.href = dataURL;
+    link.download = imageName;
+    link.click();
   });
-});
+}
